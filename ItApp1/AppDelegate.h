327eafa1da48d7516b44c25a29db375e7a7c17b4
QUAQUA <quaqua@quaquademacbook-air.local>
2017-10-27 10:00:26 +0800

    Initial Commit

---
+++ ItApp1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ItApp1
+//
+//  Created by QUAQUA on 2017/10/27.
+//  Copyright © 2017年 zws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ItApp1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ItApp1
+//
+//  Created by QUAQUA on 2017/10/27.
+//  Copyright © 2017年 zws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
